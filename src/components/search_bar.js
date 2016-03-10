948675ceb55e11c8b9c67b0079665307de4537d0
Shadid Chowdhury <shadid.chowdhury@smartbear.com>
2016-03-10 22:49:56 +0100

    Introduce state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,20 @@ import React, {Component} from 'react'
 
 class SearchBar extends Component {
 
+    constructor(props){
+
+        super(props);
+        this.state = {term: ''};
+    }
+
     render() {
-        return <input onChange={event => console.log(event.target.value)}/>;
+
+        return (
+            <div>
+                <input onChange={event => {this.setState({term: event.target.value})}}/>
+            the input is {this.state.term}
+            </div>
+                );
     }
 
 
