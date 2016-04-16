d8b8656d7ab0dafa34e22d8ec9684d2939e95fcb
Shadid Chowdhury <shadid.chowdhury@smartbear.com>
2016-04-16 23:55:13 +0200

    Use es6 syntax

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {videos: []};
-        YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
-            this.setState({videos: data})
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({videos})
         });
 
     }
