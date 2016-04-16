0f967e5aa1c03c5a183b2a39d3e521f21a214783
Shadid Chowdhury <shadid.chowdhury@smartbear.com>
2016-04-16 23:47:28 +0200

    Change to component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
+import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyDKYMIOM8uM4VKHvVMAX6A3gJHVQmP0SIA';
-const App = () => {
-    return (<div>
-        <SearchBar />
-    </div>);
+YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
+    console.log(data);
+});
+
+class App extends Component {
+
+    render() {
+        return (<div>
+            <SearchBar />
+        </div>);
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
