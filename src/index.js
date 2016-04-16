2d14e47679e2029bca634f5c41899d43a69414bf
Shadid Chowdhury <shadid.chowdhury@smartbear.com>
2016-04-16 23:53:46 +0200

    Do the search in the constructo

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import SearchBar from './components/search_bar'
 import YTSearch from 'youtube-api-search'
 
 const API_KEY = 'AIzaSyDKYMIOM8uM4VKHvVMAX6A3gJHVQmP0SIA';
-YTSearch({key: API_KEY, term: 'surfboards'}, function (data) {
-    console.log(data);
-});
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+        YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
+            this.setState({videos: data})
+        });
+
+    }
 
     render() {
         return (<div>
