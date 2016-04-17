50b27bcefe25d988608582fa1ae8a5251d16f40e
Shadid Chowdhury <shadid.chowdhury@smartbear.com>
2016-04-17 14:52:25 +0200

    Add video player

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import SearchBar from './components/search_bar'
 import YTSearch from 'youtube-api-search'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyDKYMIOM8uM4VKHvVMAX6A3gJHVQmP0SIA';
 
@@ -20,6 +21,8 @@ class App extends Component {
     render() {
         return (<div>
             <SearchBar />
+            <VideoDetail video={this.state.videos[0]}/>
+
             <VideoList videos={this.state.videos}/>
         </div>);
     }
